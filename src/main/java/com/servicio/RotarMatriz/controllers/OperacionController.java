27c6dcded13465677d41833f19ae3bc3a0b3dac5
Oliver RJ <ing.orodriguezjimenez@gmail.com>
2022-05-06 17:13:10 -0500

    Creacion de segunda forma para consumir matriz 2D

--- src/main/java/com/servicio/RotarMatriz/controllers/OperacionController.java
+++ src/main/java/com/servicio/RotarMatriz/controllers/OperacionController.java
@@ -11,7 +11,7 @@ public class OperacionController {
 
     @PostMapping(value = "/RotarMatriz", consumes = { MediaType.APPLICATION_JSON_VALUE },
             produces = { MediaType.APPLICATION_JSON_VALUE })
-    public  String[][] test2(@RequestBody  Map<String,Object> map){
+    public  String[][] RotarMatriz(@RequestBody  Map<String,Object> map){
 
         String[][] stringArray = null;
         ArrayList<ArrayList<String>> mainList = new ArrayList<ArrayList<String>>();
@@ -43,25 +43,34 @@ public class OperacionController {
         return matNueva;
 
     }
+
     @PostMapping(value = "/RotarMatriz2", consumes = { MediaType.APPLICATION_JSON_VALUE },
             produces = { MediaType.APPLICATION_JSON_VALUE })
-    public int[][] rotarCuadrado90Grados(@RequestBody int [][] matOriginal){
-        int fil = matOriginal.length;
-        int col = matOriginal[0].length;
+    public   String[][] test2(@RequestBody  String[][] stringArray){
+
+        int fil = stringArray.length;
+        int col = stringArray[0].length;
         int contFil = 0;
         int contCol = 0;
-        int[][] matNueva = new int [fil][col];
-        for (int i = fil; i >= 0; i--)
+
+        String[][] matNueva = new String [fil][col];
+        for (int i = fil-1; i >= 0; i--)
         {
+            System.out.println("i: " +i);
             contCol =  0;
-            for (int j = col; j >= 0; j--)
+            for (int j = col-1; j >= 0; j--)
             {
-                matNueva[contFil][contCol] = matOriginal[i][j];
+                matNueva[contFil][contCol] = stringArray[i][j];
                 contCol ++;
             }
             contFil ++;
+            System.out.println("contFil: " +i);
         }
+
         return matNueva;
+
+
+
     }
 
 }
